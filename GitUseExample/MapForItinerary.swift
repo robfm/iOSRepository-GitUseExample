fe1e9f3ca4d750a86b9d2bcfa79c84dc5362e429
Roberto Fernandez <robefm83@me.com>
2018-04-20 11:22:42 +0200

    Add the map to itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Rob FM on 4/20/18.
+//  Copyright © 2018 Rob FM. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
